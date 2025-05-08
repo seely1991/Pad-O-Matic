7f65afdac18e7afb7825eeae32dd2b13c7c7eb17
seely1991 <40604521+seely1991@users.noreply.github.com>
2025-05-08 14:10:21 -0600

    Update just_looper.ino

--- just_looper.ino
+++ just_looper.ino
@@ -66,7 +66,7 @@ const int FADE_DURATION_MS = 1500;
 const int BUFFER_SAMPLES = SAMPLE_RATE * (LOOP_DURATION_MS + FADE_DURATION_MS) * 1.25;
 const float signalThreshold = 0.01;
 const int silenceTimeout = 750;
-const float loopGainDecay = 0.9;
+const float loopGainDecay = 0.95;
 
 DMAMEM int16_t loopBuffer[BUFFER_SAMPLES];
 uint32_t writeIndex = 0;
